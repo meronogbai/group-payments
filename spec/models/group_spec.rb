f2faf21f24c67a60cea000ebbe2a730f16dca8cd
Meron Ogbai <okbaymeron@gmail.com>
2020-11-18 01:02:43 +0300

    Add model tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    @user = User.create(name: 'test_user')
+  end
+
+  it "doesn't validate group with short name" do
+    group = Group.new(name: 'ha', user_id: @user.id)
+    expect(group.valid?).to be false
+  end
+
+  it "doesn't validate group if user doesn't exist" do
+    group = Group.new(name: 'ha', user_id: 9)
+    expect(group.valid?).to be false
+  end
+
+  it 'validates group with name with length >= 3 and existing user' do
+    group = Group.new(name: 'haha', user_id: @user.id)
+    expect(group.valid?).to be true
+  end
 end
--- spec/models/payments_group_spec.rb
+++ spec/models/payments_group_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe PaymentsGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    @user = User.create(name: 'test_user')
+    @group = Group.create(name: 'haha', user_id: @user.id)
+    @payment = Payment.create(name: 'haha', amount: 500, author_id: @user.id)
+  end
+
+  it "doesn't validate if group doesn't exist" do
+    pgroup = PaymentsGroup.new(group_id: 1000, payment_id: @payment.id)
+    expect(pgroup.valid?).to be false
+  end
+
+  it "doesn't validate if payment doesn't exist" do
+    pgroup = PaymentsGroup.new(payment_id: 1000, group_id: @group.id)
+    expect(pgroup.valid?).to be false
+  end
+
+  it 'validates with valid info' do
+    pgroup = PaymentsGroup.new(payment_id: @payment.id, group_id: @group.id)
+    expect(pgroup.valid?).to be true
+  end
 end
