ea579ae5fde5c96357cfa099e305ef6b4a2175a8
Meron Ogbai <okbaymeron@gmail.com>
2020-11-18 15:13:46 +0300

    Add signup page and style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name])
+    if @user.save
+      flash[:notice] = 'Successful Sign Up'
+      redirect_to @user
+    else
+      flash[:alert] = 'Please use a different name'
+      redirect_back(fallback_location: signup_path)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   resources :sessions, only: %i[new create destroy]
   get 'login', to: 'sessions#new'
   delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
 end
