abc2fdfa2599138aae04853bb372dfd3d03398d9
Meron Ogbai <okbaymeron@gmail.com>
2020-11-20 00:06:18 +0300

    Add payment create functionality

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,6 +8,20 @@ class PaymentsController < ApplicationController
     @payment = Payment.new
   end
 
+  def create
+    payment = Payment.new(payment_params)
+    payment.author_id = current_user.id
+    if payment.save
+      group_id = params[:payment][:group_id]
+      PaymentsGroup.create(group_id: group_id, payment_id: payment.id) if group_id
+      flash[:success] = 'Payment added'
+      redirect_to payments_path
+    else
+      flash[:error] = 'Invalid payment'
+      redirect_back(fallback_location: payments_path)
+    end
+  end
+
   private
 
   def logged_in_or_back
@@ -15,5 +29,9 @@ class PaymentsController < ApplicationController
 
     flash[:danger] = 'Please login to view your transactions'
     redirect_to root_url
+  end
+
+  def payment_params
+    params.require(:payment).permit(:name, :amount)
   end
 end
