d62d40d30edb2d7504fca1453f87cf29301c4993
Meron Ogbai <okbaymeron@gmail.com>
2020-11-22 01:41:42 +0300

    Add group helper tests

--- spec/helpers/groups_helper_spec.rb
+++ spec/helpers/groups_helper_spec.rb
@@ -11,5 +11,17 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe GroupsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'display icon' do
+    before(:example) do
+      @user = User.create(name: 'test_user')
+      @group = Group.create(name: 'test_group', user_id: @user.id)
+    end
+    it 'displays default icon if icon is not provided' do
+      expect(display_icon(@group)).to eq(image_tag('default-group-icon.png', class: 'group-img'))
+    end
+    it 'displays custom icon if icon is provided' do
+      @group.icon.attach(io: File.open('spec/test.png'), filename: 'test.png', content_type: 'image/png')
+      expect(display_icon(@group)).not_to eq(image_tag('default-group-icon.png', class: 'group-img'))
+    end
+  end
 end
