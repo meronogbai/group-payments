cb194842b571c3eb05abfd4d10025ee089823500
Meron Ogbai <okbaymeron@gmail.com>
2020-11-22 01:48:30 +0300

    Add test for payments helper

--- spec/helpers/payments_helper_spec.rb
+++ spec/helpers/payments_helper_spec.rb
@@ -11,5 +11,16 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe PaymentsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'total payments' do
+    before(:example) do
+      user = User.create(name: 'test_user')
+      5.times do |i|
+        Payment.create(name: "test_payment_#{i}", amount: i, author_id: user.id)
+      end
+      @payments = Payment.all
+    end
+    it 'gives sum of all payments' do
+      expect(total_payments(@payments)).to eq 10
+    end
+  end
 end
