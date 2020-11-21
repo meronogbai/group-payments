8c128bbb3bf59335948da10b7bb0048f6a8be23d
Meron Ogbai <okbaymeron@gmail.com>
2020-11-22 01:51:37 +0300

    Add test for users helper

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -11,5 +11,16 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe UsersHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'display avatar' do
+    before(:example) do
+      @user = User.create(name: 'test_user')
+    end
+    it 'displays default avatar if avatar is not provided' do
+      expect(display_avatar(@user)).to eq(image_tag('default-avatar.png', class: 'avatar rounded-circle'))
+    end
+    it 'displays custom avatar if avatar is provided' do
+      @user.avatar.attach(io: File.open('spec/test.png'), filename: 'test.png', content_type: 'image/png')
+      expect(display_avatar(@user)).not_to eq(image_tag('default-avatar.png', class: 'avatar rounded-circle'))
+    end
+  end
 end
