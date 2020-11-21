7529906628ae99470f772ac902c6e97c75390b43
Meron Ogbai <okbaymeron@gmail.com>
2020-11-21 23:02:30 +0300

    Refactor img styles

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,9 +1,9 @@
 module GroupsHelper
   def display_icon(group)
     if group.icon.attached?
-      image_tag url_for(group.icon)
+      image_tag url_for(group.icon), class: 'group-img'
     else
-      image_tag 'default-group-icon.png'
+      image_tag 'default-group-icon.png', class: 'group-img'
     end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def display_avatar(user)
     if user.avatar.attached?
-      image_tag url_for(user.avatar), class: 'rounded-circle'
+      image_tag url_for(user.avatar), class: 'avatar rounded-circle'
     else
-      image_tag 'default-avatar.png', class: 'rounded-circle'
+      image_tag 'default-avatar.png', class: 'avatar rounded-circle'
     end
   end
 end
