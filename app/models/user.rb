f1f1e2303eeac7981f0580184a5270ea9534a517
Meron Ogbai <okbaymeron@gmail.com>
2020-11-22 19:50:06 +0300

    Fix user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :payments, foreign_key: 'author_id'
   has_many :groups
-  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 100 }
   has_one_attached :avatar
 end
