d7779243a82351018291f9d1dbbb14608ec520cb
Meron Ogbai <okbaymeron@gmail.com>
2020-12-18 07:13:28 +0300

    Fix back button showing protected info after logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :set_cache_headers
+  def set_cache_headers
+    response.headers['Cache-Control'] = 'no-cache, no-store'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Mon, 01 Jan 1990 00:00:00 GMT'
+  end
 
   private
 
